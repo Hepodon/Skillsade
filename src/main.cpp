86ea3f41a1cd7ff6d5879fd250ef1f06a36993a5
Hepdon <brandonlewis0120@gmail.com>
2026-02-24 22:06:01 -0600

    ad

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,25 @@
 #include "main.h"
-#include "RclTracking.hpp"
 #include "lemlib/chassis/chassis.hpp"
-#include "lemlib/chassis/trackingWheel.hpp"
-#include "liblvgl/core/lv_obj.h"
-#include "liblvgl/display/lv_display.h"
-#include "liblvgl/misc/lv_types.h"
-#include "pros/abstract_motor.hpp"
-#include "pros/adi.h"
 #include "pros/adi.hpp"
-#include "pros/distance.hpp"
 #include "pros/misc.h"
-#include "pros/motor_group.hpp"
-#include "pros/motors.h"
-#include "pros/motors.hpp"
-#include "pros/optical.hpp"
-#include <cmath>
-#include <cstdio>
 
 using namespace pros;
-using namespace std;
 
-float motorPorts[9][3] = {{-8, 0, 0}, {7, 0, 0},   {-19, 0, 0},
-                          {9, 0, 0},  {-17, 0, 0}, {3, 0, 0},
-                          {18, 0, 0}, {12, 0, 0},  {6, 0, 0}};
+MotorGroup aleft({-10, -9, -8});
+MotorGroup aright({1, 2, 3});
 
-MotorGroup aleft({static_cast<signed char>(motorPorts[0][0]),
-                  static_cast<signed char>(motorPorts[1][0]),
-                  static_cast<signed char>(motorPorts[2][0])},
-                 MotorGearset::blue, v5::MotorUnits::degrees);
-MotorGroup aright({static_cast<signed char>(motorPorts[3][0]),
-                   static_cast<signed char>(motorPorts[4][0]),
-                   static_cast<signed char>(motorPorts[5][0])},
-                  MotorGearset::blue, v5::MotorUnits::degrees);
-
-Motor intake(static_cast<signed char>(motorPorts[6][0]));
-Motor topOut(static_cast<signed char>(motorPorts[7][0]));
-Motor sorter(static_cast<signed char>(motorPorts[8][0]));
+MotorGroup middle({7, -4});
 
 adi::Pneumatics match('a', false);
 adi::Pneumatics arm('h', false);
+adi::Pneumatics tripstate('e', false);
+adi::Pneumatics tripstate2('f', false);
 
-Distance rightFront(0);
-Distance rightBack(0);
-Distance leftFront(0);
-Distance leftBack(0);
-Distance front(0);
-Distance matchDist(0);
+IMU imu(6);
 
-Rotation vertRotation(-5);
+lemlib::Drivetrain DT(&aleft, &aright, 4, 12, 12, 6);
 
-Optical colorSensorMatch();
-Optical colorSensorScore();
-
-lemlib::Drivetrain DT(&aleft, &aright, 12.72, lemlib::Omniwheel::NEW_325, 450,
-                      8);
-
-IMU inertial1(4);
-IMU inertial2(-11);
-
-lemlib::TrackingWheel leftVert(&vertRotation, lemlib::Omniwheel::NEW_2, 0.0, 1);
-
-lemlib::OdomSensors sensors(&leftVert, nullptr, nullptr, nullptr, &inertial1);
+lemlib::OdomSensors sensors(nullptr, nullptr, nullptr, nullptr, &imu);
 
 // lateral PID controller
 lemlib::ControllerSettings
@@ -90,276 +49,116 @@ lemlib::ControllerSettings
 
 lemlib::Chassis chassis(DT, lateral_controller, angular_controller, sensors);
 
-RclTracking rcl(&chassis);
-
 Controller userInput(E_CONTROLLER_MASTER);
 
-void avgIMU() {
-  while (true) {
-    inertial1.set_heading((inertial1.get_heading() + inertial2.get_heading()) /
-                          2);
-    delay(5);
-  }
-}
-
-bool leftAuton = false;
-
-void setPoseFromAllSensors() {
-  const double FIELD_W = 3658.0; // mm
-  const double FIELD_H = 3658.0; // mm
-
-  // Distance from center of bot to sensors
-  const int SIDE_OFFSET = 90.0;
-  const int FRONT_OFFSET = 85.0;
-
-  // Distance between front and back sensors
-  const int sensorSpacing = 180.0;
-
-  // Read sensors
-  double lf = leftFront.get();
-  double lb = leftBack.get();
-  double rf = rightFront.get();
-  double rb = rightBack.get();
-  double f = front.get();
-
-  // Angle from walls
-  double thetaLeft = atan2(lf - lb, sensorSpacing) * 180 / M_PI;
-  double thetaRight = atan2(rf - rb, sensorSpacing) * 180 / M_PI;
-
-  double theta = leftAuton ? thetaLeft : thetaRight;
-
-  // Heading
-  double imuHeading = inertial1.get_heading();
-  double heading = imuHeading - theta;
-
-  // X position
-  double xLeft = (lf + lb) / 2 + SIDE_OFFSET;
-  double xRight = FIELD_W - ((rf + rb) / 2 + SIDE_OFFSET);
-  double x = (xLeft + xRight) / 2;
-
-  // Y position
-  double y = FIELD_H - (f + FRONT_OFFSET);
-
-  // Convert to inches
-  chassis.setPose(x / 25.4, y / 25.4, heading);
-}
-
-enum teamColor { red, blue };
-enum where { top, middle, middleSlow };
-
-class score_State {
-
-public:
-  // Different scoring and intake states
-  score_State() {};
-  // Intake and store blocks
-  void store() {
-    topOut.move(127);
-    sorter.move(-127);
-    intake.move(-127);
-  }
-
-  // Score blocks on long goals
-  void loadTop() {
-    topOut.move(-127);
-    sorter.move(-127);
-    intake.move(-127);
-  }
-
-  // Score blocks on central upper goal
-  void loadMiddle() {
-    topOut.move(127);
-    sorter.move(127);
-    intake.move(-127);
-  }
-
-  // Score blocks on central upper goal
-  void loadMiddleSLOW() {
-    topOut.move(80);
-    sorter.move(80);
-    intake.move(-80);
-  }
-
-  // score blocks on central lower goal
-  void loadBottom() {
-    topOut.move(127);
-    sorter.move(127);
-    intake.move(127);
-  }
-  // Stop motors
-  void cancel() {
-    topOut.move(0);
-    sorter.move(0);
-    intake.move(0);
-  }
-
-  void intakeUntilOppCol(teamColor color, int timeout) {
-    store();
-    int time = 0;
-    if (color == red) {
-      while ((colorSensorMatch().get_hue() <= 15 ||
-              colorSensorMatch().get_hue() >= 345) &&
-             time < timeout) {
-        delay(10);
-        time += 10;
-      }
-    } else {
-      while ((colorSensorMatch().get_hue() <= 240 ||
-              colorSensorMatch().get_hue() >= 200) &&
-             time < timeout) {
-        delay(10);
-        time += 10;
-      }
-    }
-  }
-
-  void scoreUntilOppCol(teamColor color, int timeout, where where = top) {
-    if (where == top) {
-      loadTop();
-    } else if (where == middle) {
-      loadMiddle();
-    } else {
-      loadMiddleSLOW();
-    }
-    int time = 0;
-    if (color == red) {
-      while ((colorSensorMatch().get_hue() <= 15 ||
-              colorSensorMatch().get_hue() >= 345) &&
-             time < timeout) {
-        delay(10);
-        time += 10;
-      }
-    } else {
-      while ((colorSensorMatch().get_hue() <= 240 ||
-              colorSensorMatch().get_hue() >= 200) &&
-             time < timeout) {
-        delay(10);
-        time += 10;
-      }
-    }
-  }
-};
-
-score_State balls;
-
-void scoring() {
-  while (true) {
-
-    if (userInput.get_digital(DIGITAL_R2)) {
-      balls.store();
-    } else if (userInput.get_digital(DIGITAL_R1)) {
-      balls.loadTop();
-    } else if (userInput.get_digital(DIGITAL_B)) {
-      balls.loadMiddle();
-    } else if (userInput.get_digital(DIGITAL_A)) {
-      balls.loadBottom();
-    } else if (userInput.get_digital(DIGITAL_X)) {
-      balls.loadMiddleSLOW();
-    } else {
-      balls.cancel();
-    }
-    delay(20);
-  }
-}
-
-void screenInit() {}
-
-void diagnosticsUpdater() {
-  while (true) {
-    for (int i = 0; i < 9; i++) {
-      motorPorts[i][1] = Motor(i).get_temperature();
-      motorPorts[i][2] = Motor(i).get_position();
-    }
-  }
-}
-
-enum auton { Left, Right, rSolo, skills };
-
-auton selected;
-
-void rightAuton(lv_event_t *e) { selected = Right; }
-
-void initialize() {
-
-  lv_init();
-  ////////////////////////
-
-  lv_obj_t *autonScreen = lv_obj_create(NULL);
-  lv_obj_t *trackingScreen = lv_obj_create(NULL);
-  lv_obj_t *diagScreen = lv_obj_create(NULL);
-  lv_obj_t *uiScreen = lv_obj_create(NULL);
-
-  lv_obj_t *label = uiScreen;
-  lv_obj_t *title = uiScreen;
-
-  title = lv_label_create(uiScreen);
-  lv_label_set_text(title, "VEX RIGHT AUTON");
-  lv_obj_set_style_text_font(title, &lv_font_montserrat_30, 0);
-  lv_obj_center(title);
-
-  lv_obj_t *STARTButton = lv_obj_create(uiScreen);
-
-  lv_obj_set_size(STARTButton, 200, 50);
-
-  lv_obj_align_to(STARTButton, NULL, LV_ALIGN_CENTER, 0, 100);
-
-  lv_obj_set_style_radius(STARTButton, 20, 0);
-
-  lv_obj_add_event_cb(STARTButton, rightAuton, LV_EVENT_RELEASED, NULL);
-
-  lv_obj_set_style_bg_color(STARTButton, lv_palette_main(LV_PALETTE_GREEN),
-                            LV_STATE_DEFAULT);
-
-  lv_obj_set_style_bg_color(STARTButton, lv_palette_darken(LV_PALETTE_GREEN, 3),
-                            LV_STATE_PRESSED);
-
-  ////////////////////////
-  chassis.calibrate();
-  chassis.setBrakeMode(E_MOTOR_BRAKE_HOLD);
-  Task averagingTask(avgIMU);
-  lv_screen_load(uiScreen);
-}
+void initialize() { chassis.calibrate(); }
 
 void disabled() {}
 
 void competition_initialize() {}
 
-RclSensor F(&front, 0, 20, 0, 1);
-RclSensor LF(&leftFront, 0, 20, 0, 1);
-RclSensor RF(&rightFront, 0, 20, 0, 1);
+void autonomous() {}
 
-void autonomous() {
-  colorSensorMatch().set_led_pwm(100);
-  colorSensorScore().set_led_pwm(100);
+bool in = false;
+bool mid = false;
+bool top = false;
+bool out = false;
 
-  switch (selected) {
-  case Left:
-    break;
-  case Right:
-    rcl.startTracking();
-
-    balls.intakeUntilOppCol(red, 5000);
-    balls.cancel();
-
-    break;
-  case rSolo:
-    break;
-  case skills:
-    break;
+void scoreTop() {
+  if (!top) {
+    tripstate.extend();
+    tripstate2.extend();
+    middle.move(127);
+    top = true;
+    mid = false;
+    out = false;
+    in = false;
+  } else {
+    middle.brake();
+    top = false;
+    mid = false;
+    out = false;
+    in = false;
   }
 }
 
+void intake() {
+  if (!in) {
+    tripstate.extend();
+    tripstate2.retract();
+    middle.move(127);
+    in = true;
+    top = false;
+    mid = false;
+    out = false;
+  } else {
+    middle.brake();
+    top = true;
+    mid = false;
+    out = false;
+    in = false;
+  }
+}
+
+void scoreMid() {
+  if (!mid) {
+    tripstate.retract();
+    tripstate2.retract();
+    middle.move(127);
+    mid = true;
+    top = false;
+    out = false;
+    in = false;
+  } else {
+    middle.brake();
+    top = true;
+    mid = false;
+    out = false;
+    in = false;
+  }
+}
+
+void outtake() {
+  out = true;
+  top = false;
+  mid = false;
+  in = false;
+  while (userInput.get_digital(DIGITAL_B)) {
+    tripstate.extend();
+    tripstate2.extend();
+    middle.move(-127);
+  }
+  middle.brake();
+  
+  out = false;
+  top = false;
+  mid = false;
+  in = false;
+}
+
 void opcontrol() {
-  // Ball management task
-  Task scoreTask(scoring);
   while (true) {
-    // Collect and store controller input
-    int leftY = userInput.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
-    int rightX = userInput.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+    chassis.arcade(userInput.get_analog(E_CONTROLLER_ANALOG_LEFT_Y),
+                   userInput.get_analog(E_CONTROLLER_ANALOG_RIGHT_X), true,
+                   0.4);
 
-    // Apply controller input for movement using split arcade controls
-    chassis.arcade(leftY, rightX, true, 0.40);
+    if (userInput.get_digital_new_press(DIGITAL_L1)) {
+      arm.toggle();
+    }
+    if (userInput.get_digital_new_press(DIGITAL_L2)) {
+      match.toggle();
+    }
 
-    delay(10);
+    if (userInput.get_digital_new_press(DIGITAL_R2)) {
+      scoreTop();
+    } else if (userInput.get_digital_new_press(DIGITAL_R1)) {
+      intake();
+    } else if (userInput.get_digital_new_press(DIGITAL_A)) {
+      scoreMid();
+    } else if (userInput.get_digital(DIGITAL_B)) {
+      outtake();
+    }
+
+    delay(5);
   }
 }
