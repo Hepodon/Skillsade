fedb382f36aff2ebf1d765245b0eb3c291b14108
Hepodon <brandonlewis0120@gmail.com>
2026-03-26 23:06:16 -0500

    34

--- src/portDef.cpp
+++ src/portDef.cpp
@@ -58,16 +58,17 @@ lemlib::ControllerSettings
 
 // angular PID controller
 lemlib::ControllerSettings
-    angular_controller(4.45,    // proportional gain (kP)
-                       0,       // integral gain (kI)
-                       1.28512, // derivative gain (kD)
-                       3,       // anti windup
-                       1,       // small error range, in degrees
-                       100,     // small error range timeout, in milliseconds
-                       3,       // large error range, in degrees
-                       500,     // large error range timeout, in milliseconds
-                       0        // maximum acceleration (slew)
+    angular_controller(4.45, // proportional gain (kP)
+                       0,    // integral gain (kI)
+                       30,   // derivative gain (kD)
+                       3,    // anti windup
+                       1,    // small error range, in degrees
+                       100,  // small error range timeout, in milliseconds
+                       3,    // large error range, in degrees
+                       500,  // large error range timeout, in milliseconds
+                       0     // maximum acceleration (slew)
     );
+
 lemlib::ExpoDriveCurve driveCurve(3, 15, 1.015);
 
 lemlib::Chassis chassis(DT, lateral_controller, angular_controller, sensors,
